0ed17f2505515dc72e4e89d4a4ea2cb92c3e55d0
Prathmesh Aghao <89900988+prathmeshaghao@users.noreply.github.com>
2025-03-04 10:40:19 +0530

    Assignment Day 7
    1. You are developing an Employee Management System in C#. The system
    has an Employee class that stores employee details, but it does not have a
    method to calculate the years of experience based on the joining date.
    (Use DateTime struct properties to extract year from date) write a c#
    program for the same
    2. You are working on an e-commerce application that has a list of products.
    Each product has properties: ProductID, Name, Category, and Price. use
    LINQ to find all products in the "Electronics" category that cost more
    than Rs1000?
    3. How would you use LINQ to find the most expensive product in the list?

---
+++ AssignmentDay7/AssignmentDay7/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+namespace AssignmentDay7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // For 1st Ques
+            Console.WriteLine("\n ===== 1st Question =====");
+            Employee emp = new Employee("Prathmesh Aghao", new DateTime(2022, 2, 18));
+            Console.WriteLine($"Joining year of {emp.Name} is {emp.JoiningDate.Year} and he {emp.getHowMuchExp()} years of Total Experience");
+
+
+            Console.WriteLine("\n ===== 2nd Question =====");
+            //For 2nd Ques
+            List<Product> products = new List<Product>
+            {
+
+            new Product(1, "PC", "Electronics", 50000),
+            new Product(2, "Laptop", "Electronics", 3000),
+            new Product(3, "Book", "Book", 900),
+            new Product(4, "Pen", "Stationary", 20),
+            new Product(5, "Paper", "Stationary", 300),
+           
+            };
+
+            var expensiveProd = products
+                .Where(p => p.Category == "Electronics" && p.Price >= 1000)
+                .ToList();
+
+            Console.WriteLine("\nElectronics Products costing more than Rs 1000:");
+
+            foreach (var product in expensiveProd)
+            {
+                Console.WriteLine($"{product.Name} = Rs {product.Price} with Product Id : {product.ProductID}");
+            }
+
+            Console.WriteLine("\n ===== 3rd Question =====");
+
+
+            var mostExpensiveProd = products.OrderByDescending(p => p.Price).FirstOrDefault();
+
+            if (!mostExpensiveProd.Equals(default(Product)))
+            {
+                Console.WriteLine($"\nMost Expensive Product is {mostExpensiveProd.Name} " +
+                    $"and its price is Rs {mostExpensiveProd.Price}");
+            }
+
+        }
+    }
+}
