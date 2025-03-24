e0384dca1eac5fb1ff9a40eba3e6170f75e22537
Prathmesh Aghao <89900988+prathmeshaghao@users.noreply.github.com>
2025-03-24 15:33:32 +0530

    Add files via upload

---
+++ AssignmentDay3/AssignmentDay3/1stQ.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace AssignmentDay3
+{
+    class Car
+    {
+        
+        private int carID;
+        private string brand;
+        private string model;
+        private int year;
+        private double price;
+
+       
+        public int CarID
+        {
+            get { return carID; }
+            set { carID = value; }
+        }
+
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        
+        public Car(int carID, string brand, string model, int year, double price)
+        {
+            Console.WriteLine("Receiving Car Information");
+            CarID = carID;
+            Brand = brand;
+            Model = model;
+            Year = year;
+            Price = price;
+        }
+
+        public Car()
+        {
+            Console.WriteLine("Receiving Car Information (Default Values)");
+            CarID = 0;
+            Brand = "Unknown";
+            Model = "Unknown";
+            Year = 2000;
+            Price = 0.0;
+        }
+
+        
+        public void PresentCarInfo()
+        {
+            Console.WriteLine("\nPresenting Car Information");
+            Console.WriteLine($"Car ID: {CarID}");
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Model: {Model}");
+            Console.WriteLine($"Year: {Year}");
+            Console.WriteLine($"Price: ${Price}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+          
+            Console.Write("Enter Car ID: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Brand: ");
+            string brand = Console.ReadLine();
+
+            Console.Write("Enter Model: ");
+            string model = Console.ReadLine();
+
+            Console.Write("Enter Manufacturing Year: ");
+            int year = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Price: ");
+            double price = double.Parse(Console.ReadLine());
+
+            Car myCar = new Car(id, brand, model, year, price);
+            myCar.PresentCarInfo();
+
+            
+            Car defaultCar = new Car();
+            defaultCar.PresentCarInfo();
+        }
+    }
+}
---
+++ AssignmentDay3/AssignmentDay3/2ndQ.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+//Sorry for the bad structuring ma'am ,I am doing every program in a folder itself so for running 2nd code i.e password chcker we have to comment the whole 1st code i.e Car Name System
+//I will take care that this wont happen for next week
+
+namespace AssignmentDay3
+{
+    internal class _2ndQ
+    {
+        static bool IsValidPassword(string password)
+        {
+            if (password.Length < 6)
+            {
+                Console.WriteLine("Password must be at least 6 characters long.");
+                return false;
+            }
+
+            if (!Regex.IsMatch(password, @"[A-Z]"))
+            {
+                Console.WriteLine("Password must contain at least one uppercase letter.");
+                return false;
+            }
+
+            if (!Regex.IsMatch(password, @"\d"))
+            {
+                Console.WriteLine("Password must contain at least one digit.");
+                return false;
+            }
+
+            return true;
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter your password: ");
+            string password = Console.ReadLine();
+
+            if (IsValidPassword(password))
+            {
+                Console.WriteLine("Password is valid.");
+            }
+            else
+            {
+                Console.WriteLine("Password is invalid. Please try again.");
+            }
+        }
+    }
+}
