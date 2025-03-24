1732ead143eb8e9be2b1c6e07aa64b905bd9cb58
Prathmesh Aghao <89900988+prathmeshaghao@users.noreply.github.com>
2025-03-24 15:34:04 +0530

    Add files via upload

---
+++ AssignmentDay4/AssignmentDay4/1st.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Xml.Linq;
+
+class loggedINUsers
+{
+    public static int totalusers = 0;
+    public string username;
+
+    public loggedINUsers(string name)
+    {
+        username = name;
+        totalusers++;
+    }
+
+    public void showUser()
+    {
+        Console.WriteLine($"User loggin in : {username}");
+    }
+
+    public static void showTotalUsers()
+    {
+        Console.WriteLine($"Total number of users loggin is :{totalusers}");
+    }
+}
+
+class Program()
+{
+    static void Main()
+    {
+        loggedINUsers user1 = new loggedINUsers("Rho");
+        user1.showUser();
+        loggedINUsers user2 = new loggedINUsers("Alpha");
+        user2.showUser();
+        loggedINUsers user3 = new loggedINUsers("Gamma");
+        user3.showUser();
+
+        loggedINUsers.showTotalUsers();
+
+    }
+}
---
+++ AssignmentDay4/AssignmentDay4/2nd.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDay4
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string name, double salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+
+
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"Employee Name is {Name} and his Salary is {Salary}");
+        }
+    }
+
+    class Manager : Employee
+    {
+        public double Bonus { get; set; }
+
+        public Manager(string name, double salary, double bonus) : base(name, salary)
+        {
+            Bonus = bonus;
+        }
+
+        public void DisplayManagerDetails()
+        {
+            Console.WriteLine($"Manager Name is {Name} and his Salary is {Salary} with a Bonus of {Bonus}");
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Employee emp = new Employee("Omkar", 80000);
+            emp.DisplayEmployeeDetails();
+
+            Manager mgr = new Manager("Vaibhav", 100000, 25000);
+            mgr.DisplayManagerDetails(); ;
+        }
+    }
+
+}
