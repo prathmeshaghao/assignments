700f1c028df6103350e274e3bb705edbc1918c6b
Prathmesh Aghao <89900988+prathmeshaghao@users.noreply.github.com>
2025-03-24 15:35:18 +0530

    Add files via upload

---
+++ AssignmentDay7/AssignmentDay7/ECOMApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDay7
+{
+
+    struct Product
+    {
+       public int ProductID { get; set; }
+       public string Name { get; set; }
+       public string Category { get; set; }
+       public double Price { get; set; }
+
+        public Product(int productID, string name, string category,double price)
+        {
+            ProductID = productID;
+            Name = name;
+            Category = category;
+            Price = price;
+        }
+    }
+    
+}
---
+++ AssignmentDay7/AssignmentDay7/EMS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDay7
+{
+    struct Employee
+    {
+        public string Name { get; set; }
+        public DateTime JoiningDate { get; set; }
+
+        public Employee(string name, DateTime joindate)
+        {
+            Name = name;
+            JoiningDate = joindate;
+        }
+
+        public int getHowMuchExp()
+        {
+            return DateTime.Now.Year - JoiningDate.Year;
+        }
+    }
+   
+}
