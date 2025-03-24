e4b4885de79ad97ffad1cb896080477d7870bffb
Prathmesh Aghao <89900988+prathmeshaghao@users.noreply.github.com>
2025-03-24 15:29:46 +0530

    Add files via upload

---
+++ AssignmentDay1/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Enter Student Name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter Student Age: ");
+        int age;
+        if (!int.TryParse(Console.ReadLine(), out age))
+        {
+            Console.WriteLine("Invalid input! Age must be a numeric value.");
+            return; 
+        }
+
+        Console.Write("Enter Student Percentage: ");
+        float percentage;
+        if (!float.TryParse(Console.ReadLine(), out percentage))
+        {
+            Console.WriteLine("Invalid input! Percentage must be a numeric value.");
+            return;
+        }
+
+        Console.Write("Enter Student Email: ");
+        string email = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(email))
+        {
+            Console.WriteLine("Email cannot be empty.");
+            return;
+        }
+
+
+        Console.WriteLine("\nStudent Details:");
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Age: {age}");
+        Console.WriteLine($"Percentage: {percentage}%");
+        Console.WriteLine($"Email: {email}");
+
+
+        Console.Write("\nEnter Patient Discharge Date or press Enter keyy if not discharged: ");
+        string dischargeInput = Console.ReadLine();
+        DateTime? dischargeDate = string.IsNullOrWhiteSpace(dischargeInput) ? (DateTime?)null : DateTime.Parse(dischargeInput);
+
+        Console.WriteLine("\nPatient Discharge Status:");
+        Console.WriteLine(dischargeDate.HasValue ? $"Discharged on: {dischargeDate.Value:yyyy-MM-dd}" : "Not Discharged");
+
+
+
+    }
+}
